ab6c16c76f07162c93d2924dd38d11f171a69d99
Ryan <mayue3434@gmail.com>
2017-11-20 23:23:35 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {createStore} from 'redux';
+import {counter} from './index.redux';
+
+const store = createStore(counter);
+
+const render = () =>
+  ReactDOM.render(
+    <App store={store}/>,
+    document.getElementById('root')
+  );
+render();
+
+store.subscribe(render);
---
+++ src/index.redux.js
@@ -0,0 +1,16 @@
+const ADD_GUN = '加机关枪';
+const REMOVE_GUN = '减机关枪';
+
+export const counter = (state = 0, action) => {
+  switch (action.type) {
+    case '加机关枪':
+      return state + 1;
+    case '减机关枪':
+      return state - 1;
+    default:
+      return 10;
+  }
+};
+
+export const addGUN = () => ({type: ADD_GUN});
+export const removeGUN= () => ({type: REMOVE_GUN});
